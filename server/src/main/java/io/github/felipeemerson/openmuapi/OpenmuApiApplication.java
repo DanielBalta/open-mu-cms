b6f17357d26ccc3f04ee0c3203dec9098eac5e75
Daniel Balt <baltazar651@gmail.com>
2026-03-05 22:42:08 -0600

    se cambia imagen de docker para server por una multiplataforma

--- server/src/main/java/io/github/felipeemerson/openmuapi/OpenmuApiApplication.java
+++ server/src/main/java/io/github/felipeemerson/openmuapi/OpenmuApiApplication.java
@@ -1,5 +1,6 @@
 package io.github.felipeemerson.openmuapi;
 
+import io.github.felipeemerson.openmuapi.configuration.PreDBConfigListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +11,9 @@ public class OpenmuApiApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		SpringApplication.run(OpenmuApiApplication.class, args);
+		SpringApplication app = new SpringApplication(OpenmuApiApplication.class);
+		app.addListeners(new PreDBConfigListener());
+		app.run(args);
 	}
 
 }
