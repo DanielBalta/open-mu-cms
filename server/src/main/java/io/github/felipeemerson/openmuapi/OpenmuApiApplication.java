5db8adf9587b088153a21733bfdac36d5acc24c6
Daniel Balt <baltazar651@gmail.com>
2026-03-06 11:58:45 -0600

    se revierten cambios y se hacen ajustes de ruteo

--- server/src/main/java/io/github/felipeemerson/openmuapi/OpenmuApiApplication.java
+++ server/src/main/java/io/github/felipeemerson/openmuapi/OpenmuApiApplication.java
@@ -1,6 +1,5 @@
 package io.github.felipeemerson.openmuapi;
 
-import io.github.felipeemerson.openmuapi.configuration.PreDBConfigListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,9 +10,7 @@ public class OpenmuApiApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		SpringApplication app = new SpringApplication(OpenmuApiApplication.class);
-		app.addListeners(new PreDBConfigListener());
-		app.run(args);
+		SpringApplication.run(OpenmuApiApplication.class, args);
 	}
 
 }
